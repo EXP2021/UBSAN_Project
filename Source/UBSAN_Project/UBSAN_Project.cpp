e7a473b162d7f3f79b569d2f86875f047c543c04
solhwi <psh50zmfhtm@naver.com>
2024-11-27 21:45:37 +0900

    c++ 클래스 프로젝트로 변환

---
+++ Source/UBSAN_Project/UBSAN_Project.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UBSAN_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UBSAN_Project, "UBSAN_Project" );
